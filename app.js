b379272d367ce6f4660c1e42642ad98646e847c9
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-01-24 15:44:34 -0700

    Setup comments for guidance added

---
+++ app.js
@@ -0,0 +1,12 @@
+// Description: This is the main entry point of the application
+//Require the express module
+
+//Create an express application
+
+//Set the port to 3000
+
+//Use express.json() to parse JSON bodies
+
+//GET route for the root of the API
+
+//Set up server to listen on port 3000
