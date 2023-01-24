fac124d42415c23da814297bafd895542ebbb73b
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-01-24 15:56:30 -0700

    Remove Part II code

--- app.js
+++ app.js
@@ -13,13 +13,9 @@ app.use(express.json());
 
 //Require routes from files
 const definitionRoutes = require("./routes/definitionRoutes");
-const characterRoutes = require("./routes/characterRoutes");
 
 //Use /definitions as the base route for definition routes
 app.use("/definitions", definitionRoutes);
-
-//Use /api as the base route for character routes
-app.use("/api", characterRoutes);
 
 //Set up server to listen on port 3000
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
