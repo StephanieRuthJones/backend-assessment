75c2fef4c596fe39942f2b36c1cf42311c6053c3
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-01-31 08:37:42 -0700

    Create full template

--- config/db.js
+++ config/db.js
@@ -1,16 +1,3 @@
 //Used to connect to MongoDB database
 //Import mongoose
 const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  //Connect to MongoDB database
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
